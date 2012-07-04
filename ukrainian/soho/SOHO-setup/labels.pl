d6515f8d1d5526beed9285d43e9922d9aa533e5a
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2012-07-04 09:47:22 +0900

    soho, vi

---
+++ ukrainian/soho/SOHO-setup/labels.pl
@@ -0,0 +1,117 @@
+# LaTeX2HTML 2K.1beta (1.47)
+# Associate labels original text with physical files.
+
+
+$key = q/cite_ipforward/;
+$external_labels{$key} = "$URL/" . q|node57.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_fetchmail/;
+$external_labels{$key} = "$URL/" . q|node57.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ipchains/;
+$external_labels{$key} = "$URL/" . q|node57.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_diald/;
+$external_labels{$key} = "$URL/" . q|node57.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/modem-connection/;
+$external_labels{$key} = "$URL/" . q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/one-netcard/;
+$external_labels{$key} = "$URL/" . q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/network-layout/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ipmasq/;
+$external_labels{$key} = "$URL/" . q|node57.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/private-ip/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_natd/;
+$external_labels{$key} = "$URL/" . q|node57.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/two-netcards/;
+$external_labels{$key} = "$URL/" . q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_pppoe/;
+$external_labels{$key} = "$URL/" . q|node57.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_connect/;
+$external_labels{$key} = "$URL/" . q|node57.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_privateip/;
+$external_labels{$key} = "$URL/" . q|node57.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/security/;
+$external_labels{$key} = "$URL/" . q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_deb/;
+$external_labels{$key} = "$URL/" . q|node57.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_macosxorg-natd/;
+$external_labels{$key} = "$URL/" . q|node57.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_dns/;
+$external_labels{$key} = "$URL/" . q|node57.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_pppd/;
+$external_labels{$key} = "$URL/" . q|node57.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_nat/;
+$external_labels{$key} = "$URL/" . q|node57.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2K.1beta (1.47)
+# labels from external_latex_labels array.
+
+
+$key = q/private-ip/;
+$external_latex_labels{$key} = q|2.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/modem-connection/;
+$external_latex_labels{$key} = q|2.2.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/one-netcard/;
+$external_latex_labels{$key} = q|2.2.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/two-netcards/;
+$external_latex_labels{$key} = q|2.2.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/security/;
+$external_latex_labels{$key} = q|6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/network-layout/;
+$external_latex_labels{$key} = q|2.1|; 
+$noresave{$key} = "$nosave";
+
+1;
+
