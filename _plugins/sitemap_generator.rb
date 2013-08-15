d9df094b386484fe06045bd450416f7daeefbf93
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-08-15 19:14:47 +0900

    Fix typo

--- _plugins/sitemap_generator.rb
+++ _plugins/sitemap_generator.rb
@@ -50,13 +50,13 @@ module Jekyll
 
   # Any files that include posts, so that when a new post is added, the last
   # modified date of these pages should take that into account
-  PAGES_INCLUDE_POSTS = ["index.html"]
+  PAGES_INCLUDE_POSTS = ["index.html", "posts.html"]
 
   # Custom variable names for changefreq and priority elements
   # These names are used within the YAML Front Matter of pages or posts
   # for which you want to include these properties
-#  CHANGE_FREQUENCY_CUSTOM_VARIABLE_NAME = "change_frequency"
-#  PRIORITY_CUSTOM_VARIABLE_NAME = "priority"
+  CHANGE_FREQUENCY_CUSTOM_VARIABLE_NAME = "changefreq"
+  PRIORITY_CUSTOM_VARIABLE_NAME = "priority"
 
   class Post
     attr_accessor :name
