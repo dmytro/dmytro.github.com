572142430fbf37bc58bda40c6ef5ea2a84667729
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-10-21 19:47:37 +0900

    Reformat, bugfix: keep non-cyrillic letters.

--- _plugins/transliterate_ua.rb
+++ _plugins/transliterate_ua.rb
@@ -1,85 +1,44 @@
 # -*- coding: utf-8 -*-
+# Ukrainian transliteration for Jekyll
 module Jekyll
-  module TransliterateUA
+  class UA
+    MAP = { 
+      'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'h', 'ґ' => 'g', 'д' => 'd', 
+      'е' => 'e', 'є' => 'ya', 'ж' => 'zh', 'з' => 'z', 'и' => 'y',
+      'і' => 'i', 'й' => 'j', 'ї' => 'yi', 'к' => 'k', 'л' => 'l', 
+      'м' => 'm', 'н' => 'n', 'о' => 'o', 'п' => 'p', 'р' => 'r', 
+      'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'kh', 
+      'ц' => 'ts', 'ч' => 'ch', 'ш' => 'sh', 'щ' => 'shch', 
+      'ю' =>'yu', 'я' => 'ya',
 
-    LETTERS = { 
-
-      'а' => 'a',
-      'б' => 'b',
-      'в' => 'v',
-      'г' => 'h',
-      'ґ' => 'g',
-      'д' => 'd',
-      'е' => 'e',
-      'є' => 'ya',
-      'ж' => 'zh',
-      'з' => 'z',
-      'и' => 'y',
-      'і' => 'i',
-      'й' => 'j',
-      'ї' => 'yi',
-      'к' => 'k',
-      'л' => 'l',
-      'м' => 'm',
-      'н' => 'n',
-      'о' => 'o',
-      'п' => 'p',
-      'р' => 'r',
-      'с' => 's',
-      'т' => 't',
-      'у' => 'u',
-      'ф' => 'f',
-      'х' => 'kh',
-      'ц' => 'ts',
-      'ч' => 'ch',
-      'ш' => 'sh',
-      'щ' => 'shch',
-      'ю' => 'yu',
-      'я' => 'ya',
-
-      'А' => 'A',
-      'Б' => 'B',
-      'В' => 'V',
-      'Г' => 'H',
-      'Ґ' => 'G',
-      'Д' => 'D',
-      'Е' => 'E',
-      'Є' => 'YA',
-      'Ж' => 'ZH',
-      'З' => 'Z',
-      'И' => 'Y',
-      'І' => 'I',
-      'Й' => 'J',
-      'Ї' => 'YI',
-      'К' => 'K',
-      'Л' => 'L',
-      'М' => 'M',
-      'Н' => 'N',
-      'О' => 'O',
-      'П' => 'P',
-      'Р' => 'R',
-      'С' => 'S',
-      'Т' => 'T',
-      'У' => 'U',
-      'Ф' => 'F',
-      'Х' => 'KH',
-      'Ц' => 'TS',
-      'Ч' => 'CH',
-      'Ш' => 'SH',
-      'Щ' => 'SHCH',
-      'Ю' => 'YU',
-      'Я' => 'YA',
-      "\s" => '_'
+      'А' => 'A', 'Б' => 'B', 'В' => 'V', 'Г' => 'H', 'Ґ' => 'G', 
+      'Д' => 'D', 'Е' => 'E', 'Є' => 'YA', 'Ж' => 'ZH', 'З' => 'Z',
+      'И' => 'Y', 'І' => 'I', 'Й' => 'J', 'Ї' => 'YI', 'К' => 'K', 
+      'Л' => 'L', 'М' => 'M', 'Н' => 'N', 'О' => 'O', 'П' => 'P', 
+      'Р' => 'R', 'С' => 'S', 'Т' => 'T', 'У' => 'U', 'Ф' => 'F', 
+      'Х' => 'KH', 'Ц' => 'TS', 'Ч' => 'CH', 'Ш' => 'SH', 'Щ' => 'SHCH', 
+      'Ю' => 'YU', 'Я' => 'YA'
     }
 
-    def transliterate input
+    def self.transliterate input
       return '' unless input
-      text = input.split('').map {  |x| LETTERS[x] }.join
+      # text = input.split('').map {  |x| MAP[x] }.join
+      text = input.dup
+      MAP.keys.each do |key|
+        text.gsub!(/#{key}/, MAP[key])
+      end
+
       text
     end
+    
+  end
 
+  module Transliterate
+    def transliterate input
+      ::Jekyll::UA.transliterate input
+    end
     alias t14e transliterate
   end
 end
 
-Liquid::Template.register_filter(Jekyll::TransliterateUA)
+Liquid::Template.register_filter(Jekyll::Transliterate)
